db565172aed8823a14c3107cd35f1fdd427363cb
amyjohnson4000 <73438568+amyjohnson4000@users.noreply.github.com>
2021-11-30 16:56:24 -0500

    Adds FindEmptySpace method

--- MazeSolver/MazeSolver/Program.cs
+++ MazeSolver/MazeSolver/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Maze Solver");
+        bool[] row = new bool[] { false, false, false, false, false };
+
+        int spaceLocation = FindTheEmptySpace(row);
+
+        if (spaceLocation == -1)
+            Console.WriteLine("There is no empty space");
+        else
+            Console.WriteLine("location = " + spaceLocation);
+    }
+
+    static int FindTheEmptySpace(bool[] row)
+    {
+        int spaceLocation = -1;
+
+        for (int i = 0; i < row.Length; i++)
+        {
+            if (row[i] == true)
+            {
+                spaceLocation = i;
+                return (spaceLocation);
+            }
+        }
+
+        return spaceLocation;
+    }
+}
